ed5eab458cbdf00903f118a5088d249858859154
unknown <abiramirajasekar002@gmail.com>
2023-08-04 18:28:20 +0530

    4th commit

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const express = require("express");
 const mongoose =  require("mongoose");
 const routes = require("../router/auth.route");
 
+const cors = require('cors');
 
 
 
 const app = express()
+app.use(cors());
 app.use(express.json())
 app.use("/api",routes)
 
